828a9abc4c3a78120d455ae4a04c1dcbb24505d8
Khubaib <mohd.khubaib.cs28@iilm.edu>
2026-05-30 18:10:14 +0530

    DATATYPE

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all JS code as modern and secure, prevents accidental globals and other common mistakes
+// alert("hello")/ / we are using nodejs, not a browser, so alert is not available
+console.log(3+3); //code readable  should be high 
+console.log("Khubaib") // string concatenation
+
+let name = "khubaib"
+let age = "18"
+let isloggedIn = false
+let state;
+// number => 2 to power 53
+//bigint
+// string => sequence of characters""
+// boolean => true or false
+// null => empty value
+// undefined => value is not assigned
+// symbol => unique identifier
+
+
+
+// object
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
